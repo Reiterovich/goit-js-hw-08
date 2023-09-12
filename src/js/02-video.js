b309060612b7c1601645b14f7d43f95214b29513
Reiterovich <reiterovich.andriy@gmail.com>
2023-09-12 21:50:30 +0300

    add 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const cTime = localStorage.getItem('videoplayer-current-time');
+console.log(cTime);
+
+player
+  .setCurrentTime(cTime)
+  .then(function (seconds) {
+    // ???
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
