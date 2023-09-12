cd60dfeb94f19284244786d6e637220120c352c6
Reiterovich <reiterovich.andriy@gmail.com>
2023-09-12 15:03:56 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
+// // Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// // Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above (вище) this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Change code below (під) this line
 
 console.log(galleryItems);
+
+const galleryCont = document.querySelector('.gallery');
+const galleryFun = createImg(galleryItems);
+
+galleryCont.insertAdjacentHTML('afterbegin', galleryFun);
+
+function createImg(galleryItems) {
+  const imgEl = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
+    })
+    .join('');
+
+  return imgEl;
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
